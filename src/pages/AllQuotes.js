ace2e5f680603e319f9afe16dc1d08d67ad35142
Zucas <PaivaTech@hotmail.com>
2021-08-30 22:29:40 +0100

    Set comment's functions

--- src/pages/AllQuotes.js
+++ src/pages/AllQuotes.js
@@ -1,16 +1,34 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import useHttp from '../hooks/use-http';
+import { getAllQuotes } from '../lib/api';
 
 import QuoteList from '../components/quotes/QuoteList';
-import { Link } from 'react-router-dom';
-
-const DUMMY_QUOTES = [
-    {id: 'q1', author: 'Moni', text: 'Learning React is fun!'},
-    {id: 'q2', author: 'Monique', text: 'Learning React is great!'},
-];
+import LoadingSpinner from '../components/UI/LoadingSpinner';
+import NoQuotesFound from '../components/quotes/NoQuotesFound';
 
 const AllQuotes = () => {
+    const { sendRequest, status, data: loadedQuotes, error } = useHttp(getAllQuotes, true);
+
+    useEffect(() => {
+        sendRequest();
+    }, [sendRequest]);
+
+    if (status === 'pending') {
+        return <div className='centered'>
+            <LoadingSpinner />
+        </div>
+    }
+
+    if (error) {
+        return <p className='centered focused'>{error}</p>
+    }
+
+    if (status === 'completed' && (!loadedQuotes || loadedQuotes.length === 0)) {
+        return <NoQuotesFound />;
+    }
+
     return (
-        <QuoteList quotes={DUMMY_QUOTES} />        
+        <QuoteList quotes={loadedQuotes} />        
     )
 }
 
